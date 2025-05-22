79d1496375fa136ded6c2212bc4d8a41c6ac735e
mmoha.Muna <mmoha.munamohamed2890@gmail.com>
2025-05-22 18:09:26 -0400

    added more code

--- SBA316.js
+++ SBA316.js
@@ -1,1 +1,9 @@
+const pageTitle = document.getElementsByTagName('title')[0]
+pageTitle.textContent = "Poetry Sanctuary";
+
+let title = document.getElementById("title")
+
+let h3 = document.createElement('h3');
+h3.innerText = 'Poems Soothes The Soul';
+document.querySelector('main').appendChild(h3)
 
