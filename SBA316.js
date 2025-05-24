ff3be922b20cd9abd0f9cdf40eabb2af828e1aab
mmoha.Muna <mmoha.munamohamed2890@gmail.com>
2025-05-23 22:23:01 -0400

    added more code

--- SBA316.js
+++ SBA316.js
@@ -8,22 +8,44 @@ h3.innerText = 'Poems Soothes The Soul';
 document.querySelector('main').appendChild(h3)
 
 function submitPoetry(e) {
-    e.preventDefault()
+  e.preventDefault()
   alert("The poem was submitted");
 }
 document.getElementById("poem_content").addEventListener("submit", submitPoetry);
 
-let list = ['Poets','Poems', 'Arthurs', 'Literature'];
+let list = ['Poets', 'Poems', 'Arthurs', 'Literature'];
 
 const dFrag = document.createDocumentFragment();
-for(let p in list) {
-    const li = document.createElement('li');
-    li.textContent = list[p];
-    dFrag.appendChild(li);
+for (let p in list) {
+  const li = document.createElement('li');
+  li.textContent = list[p];
+  dFrag.appendChild(li);
 }
 document.querySelector('h3').appendChild(dFrag);
 
+let form = document.getElementById('poem_content');
+form.addEventListener('click', submitPoetry());
+form.addEventListener('mouseover', submitPoetry());
+form.addEventListener('mouseout', submitPoetry());
+
+function submitPoetry(){
+ document.getElementById('poem_content').innerHTML += 'Clicked!<br>';
+ h1.addEventListener('click', submitPoetry())
+}
+
+function submitPoetry(){
+  document.getElementById('poem_content').innerHTML += 'Moused over!<br>';
+}
+
+function submitPoetry(){
+  document.getElementById('poem_content').innerHTML += 'Moused out!<br>';
+}
+
 document.getElementById("poem_content").parentElement.nextElementSibling
 
-// document.getElementById('title').innerHTML = document.documentElement.innerHTML;
+// document.getElementById('poem_content').innerHTML += 'Clicked!<br>';
+// document.getElementById('poem_content').innerHTML += 'Moused over!<br>';
+// document.getElementById('poem_content').innerHTML += 'Moused out!<br>';
 
+// let changeFontColor = e.target;
+//   changeFontColor.textContent = 'clicked';
